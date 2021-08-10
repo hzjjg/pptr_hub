1a79dbed70adc1076662ab0f05873340932f6abf
hzjjg <hzjio@outlook.com>
2021-08-11 00:39:42 +0800

    update: add some code

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,9 +2,16 @@ import './header.scss'
 import React from "react";
 
 export default function Header() {
+
+    console.log((window as any).electron);
+        
+
     return (
         <header className="Header">
             header
+            {
+                true ?? <div>ahhaha</div>
+            }
         </header>
     )
 }
