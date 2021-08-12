0d95af57173173eafb4e5de95b88a821718d7cdc
hzj <hzjio@outlook.com>
2021-08-12 10:50:02 +0800

    fix: shortcut url bug fix

--- src/views/main/ShortCuts/index.tsx
+++ src/views/main/ShortCuts/index.tsx
@@ -14,71 +14,51 @@ export function ShortCuts() {
         },
         {
             name: '远程相机',
-            desc: 'webrtc root',
-            url: `${doamin}/remote-control/run?feature=camera&use_webrtc=true&use_a11y=false`
+            desc: 'webrtc',
+            url: `${doamin}/remote-control/run?feature=remote_camera&use_webrtc=1`
         },
         {
             name: '远程相机',
-            desc: 'webrtc 辅助权限',
-            url: `${doamin}/remote-control/run?feature=camera&use_webrtc=true&use_a11y=true`
-        },
-        {
-            name: '远程相机',
-            desc: '345 root',
-            url: `${doamin}/remote-control/run?feature=camera&use_webrtc=false&use_a11y=false`
-        },
-        {
-            name: '远程相机',
-            desc: '345 辅助权限',
-            url: `${doamin}/remote-control/run?feature=camera&use_webrtc=false&use_a11y=true`
+            desc: '345',
+            url: `${doamin}/remote-control/run?feature=remote_camera&use_webrtc=0`
         },
         {
             name: '观察模式',
-            desc: 'webrtc root',
-            url: `${doamin}/remote-control/run?feature=remote_control&use_webrtc=true&use_a11y=false`
+            desc: 'webrtc',
+            url: `${doamin}/remote-control/run?feature=remote_screen&use_webrtc=1`
         },
         {
             name: '观察模式',
-            desc: 'webrtc 辅助权限',
-            url: `${doamin}/remote-control/run?feature=remote_control&use_webrtc=true&use_a11y=true`
-        },
-        {
-            name: '观察模式',
-            desc: '345 root',
-            url: `${doamin}/remote-control/run?feature=remote_control&use_webrtc=false&use_a11y=false`
-        },
-        {
-            name: '观察模式',
-            desc: '345 辅助权限',
-            url: `${doamin}/remote-control/run?feature=remote_control&use_webrtc=false&use_a11y=true`
+            desc: '345',
+            url: `${doamin}/remote-control/run?feature=remote_screen&use_webrtc=0`
         },
         {
             name: '远程控制',
             desc: 'webrtc root',
-            url: `${doamin}/remote-control/run?feature=remote_control&use_webrtc=true&use_a11y=false`
+            url: `${doamin}/remote-control/run?feature=remote_control&use_webrtc=1&use_a11y=0`
         },
         {
             name: '远程控制',
             desc: 'webrtc 辅助权限',
-            url: `${doamin}/remote-control/run?feature=remote_control&use_webrtc=true&use_a11y=true`
+            url: `${doamin}/remote-control/run?feature=remote_control&use_webrtc=1&use_a11y=1`
         },
         {
             name: '远程控制',
             desc: '345 root',
-            url: `${doamin}/remote-control/run?feature=remote_control&use_webrtc=false&use_a11y=false`
+            url: `${doamin}/remote-control/run?feature=remote_control&use_webrtc=0&use_a11y=0`
         },
         {
             name: '远程控制',
             desc: '345 辅助权限',
-            url: `${doamin}/remote-control/run?feature=remote_control&use_webrtc=false&use_a11y=true`
+            url: `${doamin}/remote-control/run?feature=remote_control&use_webrtc=0&use_a11y=0`
         },
     ]
 
     return (
         <div className="ShortCuts">
             {
-                shortCuts.map((item) => (
-                    <div className="ShortCuts__item" key={item.url}>
+                shortCuts.map((item, index) => (
+                    <div className="ShortCuts__item" key={index}>
                         <ShortCut name={item.name} url={item.url} desc={item.desc}></ShortCut>
                     </div>)
                 )
