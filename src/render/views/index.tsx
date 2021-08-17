6debcdce9079a32acf87f0edab7c3ab67bb94d26
hzjjg <hzjio@outlook.com>
2021-08-18 00:03:21 +0800

    add: add recoil

--- src/render/views/index.tsx
+++ src/render/views/index.tsx
@@ -4,20 +4,23 @@ import Header from "../components/Header"
 import SideBar from "../components/SideBar"
 import Task from "./task";
 import Project from "./project";
+import { RecoilRoot } from "recoil";
 
 export default function Views() {
 
     return (
-        <div className="App">
-            <SideBar></SideBar>
-            <div className="App__body">
-                <Header></Header>
-                <div className="App__content">
-                    <Task></Task>
-                    {/* <Project></Project> */}
+        <RecoilRoot>
+            <div className="App">
+                <SideBar></SideBar>
+                <div className="App__body">
+                    <Header></Header>
+                    <div className="App__content">
+                        <Task></Task>
+                        {/* <Project></Project> */}
+                    </div>
                 </div>
             </div>
-        </div>
-        
+        </RecoilRoot>
+
     )
 }
