481f186f8ede91a05cfbffbd514c457568ace4e0
hzj <hzjio@outlook.com>
2021-08-18 20:58:49 +0800

    add: add page layout component

---
+++ src/render/views/components/PageLayout/index.tsx
@@ -0,0 +1,13 @@
+import { IStyle } from '@fluentui/merge-styles'
+import React from 'react'
+
+
+const style: IStyle = {}
+
+export default function () {
+    return (
+        <div className="PageLayout">
+
+        </div>
+    )
+}
